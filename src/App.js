20a7d37082c590a377a64adfba7d325c38544ca1
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-10-03 05:09:22 +0900

    리액트 조건부 연산자 사용예시

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 function App() {
   const name = '리액트';
-  return (
-    <>
-      <h1>{name}안녕!</h1>
-      <h2>잘 작동하니</h2>
-    </>
-  );
+  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
 }
-
 
 export default App;
