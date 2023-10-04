97724f15a11157308ad34d6fa36356f54e2ca795
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-10-05 02:25:59 +0900

    리액트 인라인 스타일링 코드 예시

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
   const name = '리액트';
-  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  const style = {
+
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  };
+  return <div style={style}>{name}</div>
 }
 
 export default App;
