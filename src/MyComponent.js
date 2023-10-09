9f4808509bfae52c29089dfd6f22333fb1666520
Chae Seong Lim <pan173434@gmail.com>
2023-10-10 01:56:22 +0900

    리액트 비구조화 할당 문법 2번째 방법

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,5 @@
-const MyComponent = props => {
-    const { name, children } = props;
+const MyComponent = ({ name, children }) => {
+    
     return <div>
         안녕하세요. 제 이름은 {name}입니다. <br />
         children 값은 {children}
