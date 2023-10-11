63197c6aa0da8bfd1db6eeaeb87223fde1d0af7e
Chae Seong Lim <pan173434@gmail.com>
2023-10-12 00:04:28 +0900

    prevState 사용법

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,24 @@
 import { Component } from "react";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            number: 0
-        };
+    state = {
+        number: 0, 
+        fixedNumber: 0
     }
 
     render() {
-        const { number } = this.state; 
+        const { number, fixedNumber} = this.state; 
         return (
             <div>
                 <h1>{number}</h1>
-                <button onClick={() => { this.setState({ number: number + 1 }); 
-                }}>
-                +1        
+                <h2>바꾸지 않는 값: {fixedNumber} </h2>
+                <button onClick={() => { this.setState(prevState => {
+                    return {
+                        number: prevState.number + 1
+                    }});
+                }}
+            >
+                +1    
                 </button>
             </div>
         );
