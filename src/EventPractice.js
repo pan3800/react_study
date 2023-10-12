173a0e111be46d36d288a9f03e99ce6669643611
Chae Seong Lim <pan173434@gmail.com>
2023-10-13 00:50:43 +0900

    임의 메서드 만들기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -6,6 +6,25 @@ class EventPractice extends Component {
         message: ''
     }
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
     render() {
         return(
             <div>
@@ -13,28 +32,15 @@ class EventPractice extends Component {
                 <input 
                     type="text"
                     name="message"
-                    placeholder="아무거나 입력해 주세요."
+                    placeholder="아무거나 입력해 보세요."
                     value={this.state.message}
-                    onChange={
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
+                    onChange={this.handleChange}
                 />
-
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>확인</button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         )
     }
+
 }
 
 export default EventPractice;
