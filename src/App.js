30e91c61461c0341bb5fcf48e9542a5eb64d0b02
Chae Seong Lim <pan173434@gmail.com>
2023-10-15 00:59:01 +0900

    반복되는 컴포넌트 map() 통해서 개선

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Component } from "react";
-import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox />
-      </div>  
+      <IterationSample />
     );
   }
 }
